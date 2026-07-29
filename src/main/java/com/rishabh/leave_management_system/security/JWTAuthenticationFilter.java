dba31f0803439ddd148aed868ca765fdc1297d0c
Rishabh Singh <rishusingh417@gmail.com>
2026-07-29 18:58:04 +0530

    update by id fixed the bug in password update

--- src/main/java/com/rishabh/leave_management_system/security/JWTAuthenticationFilter.java
+++ src/main/java/com/rishabh/leave_management_system/security/JWTAuthenticationFilter.java
@@ -4,6 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,10 +16,41 @@ import java.io.IOException;
 
 @Component
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    public JWTService jwtService;
+    @Autowired
+    public UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
 
+        String jwt = authHeader.substring(7);
+        String username = jwtService.extractUsername(jwt);
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        if(jwtService.isTokenValid(jwt,userDetails)){
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+
+            SecurityContextHolder.getContext()
+                    .setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request,response);
 
     }
+
+
 }
--- src/main/java/com/rishabh/leave_management_system/service/EmployeeService.java
+++ src/main/java/com/rishabh/leave_management_system/service/EmployeeService.java
@@ -1,6 +1,5 @@
 package com.rishabh.leave_management_system.service;
 
-import com.rishabh.leave_management_system.config.PasswordConfig;
 import com.rishabh.leave_management_system.entity.Employee;
 import com.rishabh.leave_management_system.exception.EmployeeNotFoundException;
 import com.rishabh.leave_management_system.repository.EmployeeRepository;
@@ -51,7 +50,7 @@ public class EmployeeService {
             employee.setName(newEmployee.getName());
             employee.setEmail(newEmployee.getEmail());
             employee.setDepartment(newEmployee.getDepartment());
-            employee.setPassword(newEmployee.getPassword());
+            employee.setPassword(passwordEncoder.encode(newEmployee.getPassword()));
 
             employeeRepository.save(employee);
             return employee;
